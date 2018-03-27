7bd54d24b8162afe9b1c9bf5cb72337e061588c4
Angelo Jansen <angelojansen92@gmail.com>
2018-03-27 13:00:24 +0200

    Added VeiligheidDao

---
+++ ProjectData/Database/Daos/VeiligheidDao.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using ProjectData.Database.Criterias;
+using ProjectData.Database.Entities;
+
+namespace ProjectData.Database.Daos
+{
+    public class VeiligheidDao : Dao<Veiligheid, VeiligheidCriteria>
+    {
+        protected override void Create(StringBuilder query, Veiligheid instance)
+        {
+            query.Append("(Marges, ) ");
+            query.Append("VALUES (");
+            query.Append("'" + instance.Regios + "', ");
+            query.Append("'" + instance.Perioden + "', ");
+            query.Append("'" + instance.LichtAfwezig + "'");
+            query.Append(")");
+        }
+
+        protected override string SetTableName()
+        {
+            return "veiligheid";
+        }
+    }
+}

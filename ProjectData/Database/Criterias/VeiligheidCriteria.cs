07543210dcc7d3ffe4c803df367ac6c72b9c6f4d
Angelo Jansen <angelojansen92@gmail.com>
2018-03-30 15:12:27 +0200

    VeiligheidCriteria Start

--- ProjectData/Database/Criterias/VeiligheidCriteria.cs
+++ ProjectData/Database/Criterias/VeiligheidCriteria.cs
@@ -9,9 +9,26 @@ namespace ProjectData.Database.Criterias
 {
     public class VeiligheidCriteria : Criteria<Veiligheid>
     {
+        public List<string> RegioList { get; set; }
+        public string Periode { get; set; }
+        public string  { get; set; }
+
         public override void Build(StringBuilder query)
         {
+            if (RegioList != null && RegioList.Count != 0)
+            {
+                QueryBuilder.Append("Regio_Code", RegioList);
+            }
 
+            if (!string.IsNullOrEmpty(Periode))
+            {
+                QueryBuilder.Append("Perioden", Periode);
+            }
+
+     //     if (!string.IsNullOrEmpty( ))
+     //     {
+     //         QueryBuilder.Append("", );
+     //     }
         }
     }
 }
